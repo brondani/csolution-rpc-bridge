c5564b53180571724247ccb36cbff740b0397157
Daniel Brondani <daniel.brondani@arm.com>
2025-04-15 16:59:57 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # csolution-rpc-bridge
+
+Install node modules
+
+```
+npm install
+```
+
+Build
+```
+npx tsc
+```
+
+Run
+```
+node dist/server.js
+```
+
+
---
+++ src/server.ts
@@ -0,0 +1,63 @@
+import { ChildProcess, spawn } from "node:child_process";
+import express from 'express';
+import cors from 'cors';
+
+let child: ChildProcess|undefined;
+const app = express();
+const port = 3000;
+
+function launch(): Boolean {
+  child = spawn('csolution', ['rpc'], { cwd: './' });
+  if (!child || !child.pid || !child.stdout || !child.stdin) {
+      console.error(`csolution rpc launch failed`);
+      return false;
+  }
+  console.warn(`csolution rpc started`);
+
+  child.on('close', () => {
+      console.warn(`csolution rpc closed`);
+      child = undefined;
+  });
+  return true;
+}
+
+function getResponse(): Promise<string> {
+  return new Promise((resolve) => {
+    let collected = '';
+    const onData = (chunk: Buffer) => {
+      collected += chunk.toString();
+      try {
+        JSON.parse(collected);
+        child!.stdout?.off('data', onData);
+        resolve(collected);
+      } catch(error) {}
+    };
+    child!.stdout?.on('data', onData);
+  });
+}
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/rpc/*method', async (req, res) => {
+  const request = req.body;
+  console.log('request:', JSON.stringify(request));
+
+  if (!child && !launch()) {
+    res.status(400).send('csolution rpc not found');
+    return;
+  }
+
+  child?.stdin?.write(JSON.stringify(request));
+
+  const response = await getResponse();
+  console.log('csolution rpc response:', response.toString());
+
+  res.status(200).json(JSON.parse(response));
+});
+
+app.listen(port, () => {
+  console.log(`server listening on http://localhost:${port}`);
+});
+
